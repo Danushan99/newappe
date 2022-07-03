5de3d864e2c4cc88d9f129e2ee993a1398677d6b
Danushan Ravendran <it20051648@my.sliit.lk>
2022-07-03 22:29:43 +0530

    add row

---
+++ newappe/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  newappe
+//
+//  Created by Groot on 2022-07-03.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
